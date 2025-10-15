f170634a6779169c61f038ec91ab8b3e74d8b915
LuFernand0 <luizfernandocoelho13@gmail.com>
2025-10-15 18:16:34 -0300

    finale

--- simplePendulum.py
+++ simplePendulum.py
@@ -8,14 +8,17 @@ holes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
 with open('data.txt', 'r') as dataFile:
     for line in dataFile:
         value = line.split(" ")
-        valueTime = value[0]
-        valueDistance = value[1]
+        valueTime = float(value[0])
+        valueDistance = (float(value[1]) + 11.9)
         
-        time.append(float(valueTime))
-        distance.append(float(valueDistance))
+        time.append((valueTime))
+        distance.append(valueDistance)
 
-col_labels = ["Furos", "Distância (cm)", "Periodo (s)"]
-tableData = list(zip(holes, distance, time))
+distanciaMeters = [x/100 for x in distance]
+distanciaMeters = [round(d, 2) for d in distanciaMeters]
+
+col_labels = ["Furos", "Distância (m)", "Periodo (s)"]
+tableData = list(zip(holes, distanciaMeters, time))
 
 
 fig = plt.figure(figsize = (10, 5), layout='constrained')
@@ -26,7 +29,7 @@ fig.canvas.manager.set_window_title("Gráfico de Periodo por distância")
 ax = fig.add_subplot(gs[0])
 
 ax.plot(
-    distance, time,
+    distanciaMeters, time,
     color = "blue",
     linestyle = "solid",
     linewidth = 2,
@@ -36,12 +39,11 @@ ax.plot(
 )
 
 ax.set_title("Periodo X Distância do centro de massa")
-ax.set_xlabel("Distância do centro de massa (cm)")
+ax.set_xlabel("Distância do centro de massa (m)")
 ax.set_ylabel("Periodo (s)")
 
-ax.set_xlim(min(distance) - 1, max(distance) + 1)
+ax.set_xlim(min(distanciaMeters) - 0.01, max(distanciaMeters) + 0.01)
 ax.set_ylim(min(time) - 0.01, max(time) + 0.01)
-
 
 
 
@@ -50,7 +52,7 @@ ax2.axis('off')
 
 table = ax2.table(
     cellText = tableData,
-    colLabels = col_labels,
+    colLabels= col_labels,
     loc = "center",
     cellLoc = "center"
 )
@@ -64,3 +66,5 @@ plt.tight_layout()
 
 
 plt.show()
+
+# menor valor do periodo é 0,14s, exatamente quando ele exatamente na outra extremidade do pêndulo
